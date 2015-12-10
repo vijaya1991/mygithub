87f5df9a06c78738084e74720481568770b4d27a
vijaya1991 <advijayalaxmi@gmail.com>
2015-12-10 18:03:46 +0530

    my Test
    Test Desc

---
+++ Script15.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="angular.js" />
+var myapp = angular
+            .module("MyModule", [])
+            .controller("MyController", function ($scope) {
+                $scope.username = 'Aslam Nagarboudi';
+                $scope.email = 'aslam.nagarboudi@gmail.com';
+            });
